3dbdeac504b360cb452e9f90bc6c48e4c78d277e
arno2546 <arno2546@users.noreply.github.com>
2020-02-27 01:18:41 +0600

    Index with added gif

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
+<style type="text/css">
+	#title{
+		font-family:Lucida Console;
+		font-weight: bolder;
+		color: black;
+		font-size: 42px;
+	}
+	#title2{
+		font-family:Lucida Console;
+		font-weight: bolder;
+		color: white;font-size: 42px;
+	}
+	body{
+  background: url(giphy.gif) no-repeat center; 
+  background-size: cover;
+  height: 700px;
+  position: center;
+}
+</style>
 <head>
-	<title>Jahid Vai Legend</title>
+	<title>ProjectAid : Share your ideas for review and funding</title>
+	
+	
 </head>
 <body>
-
+<span id="title">Project</span><span id="title2">Aid</span>
 </body>
 </html>
