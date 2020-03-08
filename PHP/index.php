4ecaa2c45ec1ea9fc3a17dc9b0cc2c3beb487356
arno2546 <arno2546@users.noreply.github.com>
2020-03-08 07:24:44 +0600

    Log-in without php
    just css design without any logic or link  up

--- PHP/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Turjo Bhai Legend er Baap/title>
-</head>
-<body>
-
-</body>
-</html>
---
+++ PHP/log_in.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="log_in_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bellota:ital,wght@1,700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="login-form">
+      <form class="" action="index.html" method="post">
+        <h1>Log In</h1>
+        <input type="text" placeholder="Username" class="txtb">
+        <input type="password" placeholder="Password" class="txtb">
+        <input type="submit" value="Log In" class="login-btn">
+        <a href="#">Create an account</a>
+    </div>
+  </body>
+</html>
